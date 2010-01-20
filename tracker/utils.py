1108c57f0d48feec899948abf651cfc329f7f3f6
sumeet <none@none>
2010-01-20 02:17:35 -0500

    Added some more tests for the tracker.
    Best way I know of running them is with `PYTHONPATH=./ nosetests -v --with-coverage --cover-package=rtracker.tracker --with-doctest`.

--- tracker/utils.py
+++ tracker/utils.py
@@ -9,10 +9,26 @@ from common.utils import mc
 INTERVAL = 1000
 
 def compact(ip, port, ascii=False):
+	"""
+	Compact IP address and port.
+
+	>>> compact('127.0.0.1', 6667, ascii=True)
+	'7f0000011a0b'
+	>>> compact('127.0.0.1', 6667) == '7f0000011a0b'.decode('hex')
+	True
+	"""
 	compacted = struct.pack('!4sH', socket.inet_aton(ip), port)
 	return binascii.hexlify(compacted) if ascii else compacted
 	
 def expand(compacted):
+	"""
+	Expand compacted IP address and port.
+	
+	>>> expand('7f0000011a0b'.decode('hex'))
+	('127.0.0.1', 6667)
+	>>> expand('7f0000011a0b')
+	('127.0.0.1', 6667)
+	"""
 	if len(compacted) == 12:
 		compacted = binascii.unhexlify(compacted)
 	ip,port = struct.unpack('!4sH', compacted)
